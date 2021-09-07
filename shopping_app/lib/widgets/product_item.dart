1471725139aa2871a238dc36ff2c7fac69824acd
lpacheco88 <l_pacheco88@hotmail.com>
2021-09-07 16:45:05 -0300

    Dummy data

---
+++ shopping_app/lib/widgets/product_item.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imgUrl;
+
+  ProductItem(
+    this.id,
+    this.title,
+    this.imgUrl,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: Image.network(
+          imgUrl,
+          fit: BoxFit.cover,
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: IconButton(
+            icon: Icon(
+              Icons.favorite,
+              color: Theme.of(context).accentColor,
+            ),
+            onPressed: null,
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: Icon(
+              Icons.shopping_cart,
+              color: Theme.of(context).accentColor,
+            ),
+            onPressed: null,
+          ),
+        ),
+      ),
+    );
+  }
+}
